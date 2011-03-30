4eee0c9653d92ce9df8c4b43449cd3b726c385aa
j.paul daigle <jdaigle1@student.gsu.edu>
2011-03-29 21:24:15 -0400

    added profile

--- profile/profile_deeps_hypersim.rb
+++ profile/profile_deeps_hypersim.rb
@@ -1,19 +1,16 @@
 $:.unshift File.join(File.dirname(__FILE__),'..','lib')
 
-#require 'ruby-prof'
+#require 'unprof'
+#require 'unroller'
 require 'globals'
 require 'node_eep_hyper'
 require 'netgen_eep_hyper'
 require 'sim'
 
-#RubyProf.start
+Globals.new 15,15,2,22, 40
+TargetGraph.new(15,5)
 
-s1 = DeepsHyperRunningSimulator.new(DeepsHyperGraph.new(TargetGraph.new(15,5)))
-s1.set
-s1.long_sim
-
-#result = RubyProf.stop
-
-#printer = RubyProf::FlatPrinter.new(result)
-#printer.print(STDOUT, 0)
+#s1 = DeepsHyperRunningSimulator.new(DeepsHyperGraph.new(TargetGraph.new(15,5))).trace("si")
+#s1.set
+#s1.long_sim
 
